bbdbb505bb0a74a0245f9c094f0fc7639fa612c0
Barry O Sullivan <barry@tercet.io>
2018-01-25 14:20:17 +0000

    Update deploy script, remove unused deps

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -4,4 +4,4 @@ require __DIR__ . '/../src/bootstrap.php';
 
 $host = getenv('REMOTE_HOST');
 
-exec('ssh '.$host.' "cd /var/www/barryosull.com/ && git pull origin master && composer update"');
+exec('ssh '.$host.' "cd /var/www/barryosull.com/ && git pull origin master && composer install"');
