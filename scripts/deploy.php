9b7f9e6c81648be29e7f4ccd256efd90c99cd51f
Barry O Sullivan <barry@tercet.io>
2018-02-24 00:52:59 +0000

    Removed unused code

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -2,8 +2,6 @@
 
 require __DIR__ . '/../src/bootstrap.php';
 
-use Symfony\Component\Cache\Simple\FilesystemCache;
-
 $host = getenv('REMOTE_HOST');
 
-exec('ssh '.$host.' "cd /var/www/barryosull.com/ && git pull origin master && composer install && php boot.php"');
+`ssh $host "cd /var/www/barryosull.com/ && git pull origin master && composer install"`;
