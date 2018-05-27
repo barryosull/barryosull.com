709acd5a7dbea380c401b86fccb13ba8aa34dbbf
Barry O Sullivan <barry@tercet.io>
2018-05-27 20:34:16 +0100

    Add test that ensure each article is valid.

--- tests/Acceptance/ArticleTest.php
+++ tests/Acceptance/ArticleTest.php
@@ -2,20 +2,56 @@
 
 use Nekudo\ShinyBlog\ShinyBlog;
 use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
 
 class ArticleTest extends TestCase
 {
-    public function test_load_article()
-    {
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-        $_SERVER['HTTP_HOST'] = 'localhost';
-        $_SERVER['REQUEST_URI'] = '/blog/acceptance-testing-your-php-app-with-ease';
+    private $blog;
 
-        $config = require __DIR__.'/../../src/config.php';
-        $app = new ShinyBlog($config);
-        $response = $app->run();
+    private function getBlog(): ShinyBlog
+    {
+
+        if (is_null($this->blog)) {
+            $_SERVER['REQUEST_METHOD'] = 'GET';
+            $_SERVER['HTTP_HOST'] = 'localhost';
+
+            $config = require __DIR__.'/../../src/config.php';
+            $this->blog = new ShinyBlog($config);
+        }
+
+        return $this->blog;
+    }
+
+    /**
+     * @test
+     * @dataProvider getAllArticleUrls
+     */
+    public function it_loads_an_article(string $articleUri)
+    {
+        $_SERVER['REQUEST_URI'] = $articleUri;
+
+        $response = $this->getBlog()->run();
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains("<h1>Acceptance testing your PHP app with ease</h1>", $response->getBody());
+        $this->assertContains("<a href=\"/\">barryosull.com</a>", $response->getBody());
+    }
+
+    public function getAllArticleUrls()
+    {
+        $_SERVER['REQUEST_URI'] = '/blog/feed';
+
+        $response = $this->getBlog()->run();
+        $xmlString = $response->getBody();
+
+        $xml = new SimpleXMLElement($xmlString);
+
+        $urls = [];
+        foreach($xml->channel->item as $item) {
+            $urls[] = [
+                str_replace('http://localhost', '', strval($item->link))
+            ];
+        }
+
+        return $urls;
     }
 }
