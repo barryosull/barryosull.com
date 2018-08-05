25c5fb9f85cdfa1f5daec44a0280441c5ad85eb4
Barry O Sullivan <barry@tercet.io>
2018-08-05 18:14:39 +0100

    Test is passing. Barebones HTML

---
+++ Laravel/resources/views/home.blade.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112076964-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-112076964-1');
+    </script>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+    <meta charset="utf-8">
+    <title>Barry O'Sullivan Development Services</title>
+    <meta name="robots" content="index,follow" />
+    <meta name="description" content="Who am me">
+    <link rel="stylesheet" href="/themes/kiss/css/kiss.css">
+    <link rel="stylesheet" href="/themes/kiss/css/prism.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+
+</head>
+<body>
+<nav class="topnav">
+    <ul>
+        <li class="first"><a href="/">barryosull.com</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="https://www.slideshare.net/BarryOSullivan18/">Talks</a></li>
+        <li><a href="https://github.com/barryosull">Github</a></li>
+        <li class="last"><a href="https://twitter.com/barryosull">Twitter</a></li>
+        <li><a href="https://www.linkedin.com/in/barryosu/">LinkedIn</a></li>
+    </ul>
+</nav>
+<div class="content">
+    <article class="page">
+        <div class="page-content">
+
+        </div>
+    </article>
+</div>
+<div class="footer">
+    <p>barryosull.com &copy; 2018</p>
+</div>
+<script src="/themes/kiss/js/prism.js"></script>
+</body>
+</html>
---
+++ Laravel/tests/Browser/HomepageTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+
+class HomepageTest extends DuskTestCase
+{
+    /**
+     * @test
+     */
+    public function can_see_header()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->waitFor('.topnav', 1)
+                ->assertSee('barryosull.com');
+        });
+    }
+
+    // TODO: Write "see content" test
+}
