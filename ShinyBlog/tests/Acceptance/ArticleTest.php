142808530b505185feea994f30afd4a23d744d2b
Barry O Sullivan <barry@tercet.io>
2018-12-29 00:23:42 +0000

    Move towards more generic tests

--- ShinyBlog/tests/Acceptance/ArticleTest.php
+++ ShinyBlog/tests/Acceptance/ArticleTest.php
@@ -1,46 +1,30 @@
 <?php namespace Tests\Acceptance;
 
+use GuzzleHttp\Psr7\Response;
 use Nekudo\ShinyBlog\ShinyBlog;
 use PHPUnit\Framework\TestCase;
 use SimpleXMLElement;
 
 class ArticleTest extends TestCase
 {
-    private $blog;
-
-    private function getBlog(): ShinyBlog
-    {
-
-        if (is_null($this->blog)) {
-            $_SERVER['REQUEST_METHOD'] = 'GET';
-            $_SERVER['HTTP_HOST'] = 'localhost';
-
-            $config = require __DIR__.'/../../src/config.php';
-            $this->blog = new ShinyBlog($config);
-        }
-
-        return $this->blog;
-    }
-
     /**
      * @test
      * @dataProvider getAllArticleUrls
      */
     public function it_loads_an_article(string $articleUri)
     {
-        $_SERVER['REQUEST_URI'] = $articleUri;
-
-        $response = $this->getBlog()->run();
+        $response = $this->visitUrl($articleUri);
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains(">barryosull.com</a>", $response->getBody());
+
+        $this->assertContains(">barryosull.com</a>", strval($response->getBody()));
     }
 
     public function getAllArticleUrls()
     {
-        $_SERVER['REQUEST_URI'] = '/blog/feed';
+        $url = '/blog/feed';
 
-        $response = $this->getBlog()->run();
+        $response = $this->visitUrl($url);
         $xmlString = $response->getBody();
 
         $xml = new SimpleXMLElement($xmlString);
@@ -53,5 +37,36 @@ class ArticleTest extends TestCase
         }
 
         return $urls;
+    }
+
+    private function visitUrl(string $url): Response
+    {
+        $_SERVER['REQUEST_URI'] = $url;
+
+        $this->getBlog()->run();
+
+        $shinyResponse = $this->getBlog()->run();
+
+        return new Response(
+            $shinyResponse->getStatusCode(),
+            [],
+            $shinyResponse->getBody()
+        );
+    }
+
+    private static $blog;
+
+    private function getBlog(): ShinyBlog
+    {
+
+        if (is_null(self::$blog)) {
+            $_SERVER['REQUEST_METHOD'] = 'GET';
+            $_SERVER['HTTP_HOST'] = 'localhost';
+
+            $config = require __DIR__.'/../../src/config.php';
+            self::$blog = new ShinyBlog($config);
+        }
+
+        return self::$blog;
     }
 }
