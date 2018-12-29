51fcce5f2388305b4404736c300c1b88f0255509
Barry O Sullivan <barry@tercet.io>
2018-12-29 00:44:16 +0000

    Move all tests to generic code.

--- ShinyBlog/tests/Acceptance/IndexTest.php
+++ ShinyBlog/tests/Acceptance/IndexTest.php
@@ -7,12 +7,7 @@ class IndexTest extends TestCase
 {
     public function test_load_homepage()
     {
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-        $_SERVER['REQUEST_URI'] = '/';
-
-        $config = require __DIR__.'/../../src/config.php';
-        $app = new ShinyBlog($config);
-        $response = $app->run();
+        $response = AppFactory::make()->visitUrl("/");
 
         $this->assertEquals(200, $response->getStatusCode());
     }
