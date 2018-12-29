83ef245c077802df17e4ded2ffcf5a2f514a652b
Barry O Sullivan <barry@tercet.io>
2018-12-29 16:16:50 +0000

    Remove annotator functionality

--- ShinyBlog/src/AppFactory.php
+++ ShinyBlog/src/AppFactory.php
@@ -8,16 +8,7 @@ class AppFactory
 {
     public static function make($requestUri): App
     {
-        if (self::isAnnotationsApiCall($requestUri)) {
-            return self::makeAnnotationApiApp();
-        } else {
-            return self::makeBlog();
-        }
-    }
-
-    private static function isAnnotationsApiCall(string $requestUri)
-    {
-        return strpos($requestUri, ANNOTATION_API_URI) === 0;
+        return self::makeBlog();
     }
 
     private static function makeBlog(): App
@@ -29,21 +20,6 @@ class AppFactory
                 $config = require __DIR__.'/../src/config.php';
                 $blog = new ShinyBlog($config);
                 $blog->run()->respond();
-            }
-        };
-    }
-
-    private static function makeAnnotationApiApp()
-    {
-        return new class implements App
-        {
-            public function run()
-            {
-                $annotationApi = new \Annotator\Flysystem\App(
-                    ANNOTATION_API_URI,
-                    __DIR__."/../storage/annotations"
-                );
-                $annotationApi->run();
             }
         };
     }
