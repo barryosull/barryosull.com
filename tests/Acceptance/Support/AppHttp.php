34275a45222f4f8a20494dbab2d0f38adf6c9587
Barry O Sullivan <barry@tercet.io>
2018-12-31 13:39:10 +0000

    Store failing tests

--- tests/Acceptance/Support/AppHttp.php
+++ tests/Acceptance/Support/AppHttp.php
@@ -14,6 +14,18 @@ class AppHttp
 
         $client = HttpServer::makeClient();
 
-        return $client->get($url);
+        $response = $client->get($url);
+
+        if ($response->getStatusCode() != 200) {
+            $filename = strtolower(str_replace("/", "-", $url));
+            $dir = "/tmp/artifacts";
+            $path = $dir . "/" . $filename;
+            if (!is_dir($dir)) {
+                mkdir($dir);
+            }
+            file_put_contents($path, strval($response->getBody()));
+        }
+
+        return $response;
     }
 }
