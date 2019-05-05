a23baea92f704680e97a4e69db4e8009b5b7dc17
Barry O Sullivan <barry@tercet.io>
2019-05-05 23:15:05 +0100

    Remove redundant imports

--- tests/Acceptance/Support/AppHttp.php
+++ tests/Acceptance/Support/AppHttp.php
@@ -3,10 +3,7 @@ declare(strict_types=1);
 
 namespace Tests\Acceptance\Support;
 
-use Barryosull\Slim\BlogController;
-use Barryosull\Slim\ContentRepository;
 use Psr\Http\Message\ResponseInterface;
-use SimpleXMLElement;
 
 class AppHttp
 {
